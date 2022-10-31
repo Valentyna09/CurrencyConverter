8a2ee4a40c3748ef762c661b0f48493c14e9ead6
Valentyna09 <110617645+Valentyna09@users.noreply.github.com>
2022-10-31 13:33:55 +0200

    updated link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
           <div className="Footer">
             <span>
               <i className ="fa-brands fa-github"></i>
-              <a href='https://github.com/Valentyna09/converter' target = "_blanck">Open-sourse code</a> by Valentyna Rudenko
+              <a href='https://github.com/Valentyna09/CurrencyConverter' target = "_blanck">Open-sourse code</a> by Valentyna Rudenko
             </span>
           </div>
         </footer>
